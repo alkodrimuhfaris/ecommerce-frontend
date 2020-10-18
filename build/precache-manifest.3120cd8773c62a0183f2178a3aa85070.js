2dce05d2c34a36b02b7209c93f802e8ff7e29be1
alkodrimuhfaris <63344061+alkodrimuhfaris@users.noreply.github.com>
2020-10-18 17:38:06 +0700

    Add build tool

---
+++ build/precache-manifest.3120cd8773c62a0183f2178a3aa85070.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d80e9fe38b3b3c8c5811d10ac369373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb77ddca784526b34f8",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8292098bb47861a46625",
+    "url": "/static/css/main.7f6e7358.chunk.css"
+  },
+  {
+    "revision": "ecb77ddca784526b34f8",
+    "url": "/static/js/2.d3acf4d8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.d3acf4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8292098bb47861a46625",
+    "url": "/static/js/main.8663034e.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  },
+  {
+    "revision": "2bfeee0f5ae6d9ffe741518eb2d65fde",
+    "url": "/static/media/TukuIcon.2bfeee0f.png"
+  },
+  {
+    "revision": "7fde3b3dccbe20cccdbad62c1b382822",
+    "url": "/static/media/arrow.7fde3b3d.svg"
+  },
+  {
+    "revision": "cd4498667e6a46a547c598736ca1234d",
+    "url": "/static/media/ava.cd449866.jpeg"
+  },
+  {
+    "revision": "9feb5368e01d3353af0c034b401e335c",
+    "url": "/static/media/bell.9feb5368.svg"
+  },
+  {
+    "revision": "3248073b6fe90b770fdea28673a44140",
+    "url": "/static/media/carousel2.3248073b.png"
+  },
+  {
+    "revision": "2dbea7a1f0a497145178e6bf05c3edd0",
+    "url": "/static/media/filter.2dbea7a1.svg"
+  },
+  {
+    "revision": "f6b933660fe37e5fa5b6b680c99d717a",
+    "url": "/static/media/icon.f6b93366.svg"
+  },
+  {
+    "revision": "287fe94ae4e625813bddbcb740a0f04e",
+    "url": "/static/media/message.287fe94a.svg"
+  },
+  {
+    "revision": "a1ee044c8eef922468785ae92ec51079",
+    "url": "/static/media/profile.a1ee044c.jpg"
+  },
+  {
+    "revision": "ecd2d4000ed69daed4d0346c6c5bdb24",
+    "url": "/static/media/searchIcon.ecd2d400.svg"
+  },
+  {
+    "revision": "c8e6c9f1ecc396e82c84e51c04118b8f",
+    "url": "/static/media/shopping-cart.c8e6c9f1.svg"
+  },
+  {
+    "revision": "f110383e267e581becb967c6f139fb34",
+    "url": "/static/media/sortAsc.f110383e.svg"
+  },
+  {
+    "revision": "7ad9c9b5203b03fc5f6d9f4e941d2580",
+    "url": "/static/media/sortDsc.7ad9c9b5.svg"
+  },
+  {
+    "revision": "8ede59bebc79f933b6eb95dba2cbb61f",
+    "url": "/static/media/user.8ede59be.svg"
+  }
+]);
